a3d6dba9e7a9ecdf2801378ec5fa248b38a8c370
Nathalie Anneessens <77076065+nathalie-anneessens@users.noreply.github.com>
2021-07-13 20:02:25 +0200

    update CSS and JS

--- wp-content/themes/benoitchangeat-kadence-child/assets/js/script.js
+++ wp-content/themes/benoitchangeat-kadence-child/assets/js/script.js
@@ -1,12 +1,8 @@
 // ACCUEIL - LIRE LA SUITE
-function tropDeTexte()
-{
-    pave = document.getElementById("index-suite");
-    pave.style.display="none";
-}
 
 function lireLaSuite()
 {
     pave = document.getElementById("index-suite");
     pave.style.display="block";
 }
+
