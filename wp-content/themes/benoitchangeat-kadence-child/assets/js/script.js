c723686127efb66e427dd9567a01cdcb1a143f01
nathalie-anneessens <nathalie.anneessens@gmail.com>
2021-07-23 17:02:47 +0200

    WIP controlers and buttons for slides

--- wp-content/themes/benoitchangeat-kadence-child/assets/js/script.js
+++ wp-content/themes/benoitchangeat-kadence-child/assets/js/script.js
@@ -6,5 +6,54 @@ function lireLaSuite()
     pave.style.display="block";
 }
 
+// ACCUEIL - CHANGEMENT DE SLIDE
 
+function changeSlide($slide)
+{
+    switch ($slide) 
+    {
+        case avec-qui:
+            //slide demandée display grid
+            slideAvecQui = document.getElementById("slide-pour-qui");
+            slideAvecQui.style.display="grid";
+            // autres slides masquées
+            slideQuelsSujet = document.getElementById("slide-quels-sujets");
+            slideQuelsSujet.style.display="none";
 
+            slideCEstQuoi = document.getElementById("slide-cest-quoi");
+            slideCEstQuoi.style.display="none";
+
+            slideAvecQui = document.getElementById("slide-avec-qui");
+            slideAvecQui.style.display="none";
+
+            slideComment = document.getElementById("slide-comment");
+            slideComment.style.display="none";
+
+            // bouton de la slide active change de couleur
+            
+            // autres boutons prennent la class cta-bleu
+            // controleur de la slide active passe solid
+            // controleur des autres class passent regular
+            
+            break;
+        
+        case quels-sujets :
+
+            break;
+
+        case cest-quoi:
+
+            break;
+
+        case cest-quoi:
+
+            break;
+
+        case cest-quoi:
+
+            break;
+
+        default:
+            break;
+    } 
+}
